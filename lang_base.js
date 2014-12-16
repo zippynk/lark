0b12d439a759d7b2494bdd0b2d5f54d42ddb9fda
JacobEdelman <edelman.jd@gmail.com>
2014-12-16 17:25:47 -0500

    making things look nice with some styling

--- lang_base.js
+++ lang_base.js
@@ -15,28 +15,7 @@ function js_lit(lit_type){
   });
 }
 var js_string=js_lit("string")
-
-var def_parser ={
-                  parse:lark.concat_parsers(left,lark.lit("="),right),
-                  postprocessor:function(captured_vars){
-
-                    return captured_vars;}
-                };
-
-
-
-match-> or_parsers(named_parser("c",charL),named_parser("w",wildL))
-(charL | wildL):* {% function(d){return "lark.concat_parsers(["+d[0].join(",")+",])";} %}
-wildL -> "$" [a-zA-Z]:+ {% function(d){return "lark.exec_lark('"+d[1]+"')";} %}
-charL -> [^$=\\\n'] {% function(d){return "lark.lit('"+d[0]+"')";} %}
-| "\\$" {% function(d){return "lark.lit('"+"$"+"')";} %}
-| "\\=" {% function(d){return "lark.lit('"+"="+"')";} %}
-| "\\n" {% function(d){return "lark.lit('"+"\\n"+"')";} %}
-| "'" {% function(d){return 'lark.lit("\'")';} %}
-right -> (charR | wildR):* {% function(d){return "function(captured_vars){return "+d[0].join("+")+";}";} %}
-wildR -> "$" [a-zA-Z]:+ {% function(d){return "captured_vars."+d[1];} %}
-charR -> [^$=\\\n'] {% function(d){return "'"+d[0]+"'";} %}
-| "\\$" {% function(d){return "'$'";} %}
-| "\\=" {% function(d){return "'='";} %}
-| "\\n" {% function(d){return "'\\n'";} %}
-| "'" {% function(d){return '"\'"';} %}
+//so... wilds, need to be a type.... really they are var()
+//so.... everything and do conversions...
+//so, also... we need to tokenize things...namely strings and numbers ....
+//so...
--- util_grammar.ne
+++ util_grammar.ne
@@ -1,13 +1,13 @@
-parser -> left "=" right {% function(d){return eval("(function(lark){return {parse:"+d[0]+",postprocessor:"+d[2]+"};})");} %}
-left -> (charL | wildL):* {% function(d){return "lark.concat_parsers(["+d[0].join(",")+",])";} %}
-wildL -> "$" [a-zA-Z]:+ {% function(d){return "lark.exec_lark('"+d[1]+"')";} %}
+parser -> left "=" right {% function(d){return (function(lark){return {parse:d[0],postprocessor:d[2]};}));} %}
+left -> (charL | wildL):* {% function(d){return lark.concat_parsers(d[0]);} %}
+wildL -> "var(" ([a-zA-Z]:+) ")" {% function(d){return lark.exec_lark(d[1]);} %}
 charL -> [^$=\\\n'] {% function(d){return "lark.lit('"+d[0]+"')";} %}
       | "\\$" {% function(d){return "lark.lit('"+"$"+"')";} %}
       | "\\=" {% function(d){return "lark.lit('"+"="+"')";} %}
       | "\\n" {% function(d){return "lark.lit('"+"\\n"+"')";} %}
       | "'" {% function(d){return 'lark.lit("\'")';} %}
 right -> (charR | wildR):* {% function(d){return "function(captured_vars){return "+d[0].join("+")+";}";} %}
-wildR -> "$" [a-zA-Z]:+ {% function(d){return "captured_vars."+d[1];} %}
+wildR -> "var(" ([a-zA-Z]:+) ")" {% function(d){return "captured_vars."+d[1];} %}
 charR -> [^$=\\\n'] {% function(d){return "'"+d[0]+"'";} %}
       | "\\$" {% function(d){return "'$'";} %}
       | "\\=" {% function(d){return "'='";} %}
