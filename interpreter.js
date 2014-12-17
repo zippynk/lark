72febdaa6c39582081ac1fe029f8573aceba209b
JacobEdelman <edelman.jd@gmail.com>
2014-12-17 16:41:43 -0500

    prettified and updated

--- interpreter.js
+++ interpreter.js
@@ -1,10 +1,19 @@
 (function() {
+  /* You may be wondering what the difference between a rule and a parser is.
+  A parser takes some input and returns an object indicating whether it matched
+  the input and possibly including some variables it captured (specific parts
+  that it matched that may of may not have been postprocessed). A rule has a
+  parser property that is a parser and a postprocessor property that takes the
+  captured variables of the parser and returns a string. The use for this is
+  that we can check for correct parsings and only execute the postprocessor part
+  later. */
+
   var _ = require('underscore');
   var nearley = require('nearley');
   var grammar = require('./util_grammar.js');
-  /* The lit function takes a string or matchable object and returns a function
-  that checks for a complete matche of the whole string of its input with the
-  string or matchable object. */
+  /* The lit function takes a string or matchable object and returns a
+  parser function that checks for a complete matche of the whole string of its
+  input with the string or matchable object. */
   function lit(match_against) {
     /* No memoization here because I think this is fast enough without it. A
     possible change in the future may be adding memoization here or making it
@@ -105,6 +114,50 @@
     return join_parsers(left_half, right_half);
   }
 
+  /* This is for when you have a parser and want to make it so it captures
+  what it parses with a specific name and optionally a postprocessor. */
+
+  function named_parser(name, rule, postprocessor) {
+    return function(str_to_check) {
+      var attempt = rule(str_to_check);
+      if (attempt.matches) {
+        var ret = {
+          matches: true,
+          captured_vars: {}
+        };
+        if (postprocessor != undefined) {
+          ret.captured_vars[name] = postprocessor(str_to_check);
+        } else {
+          ret.captured_vars[name] = str_to_check;
+        }
+        return ret;
+      }
+      return {
+        matches: false
+      };
+    }
+  }
+
+  /* This is for when you have a function and want to turn it into a parser. */
+
+  function func_to_parser(name, func) {
+    return function(str_to_check) {
+      var attempt = func(str_to_check);
+      if (attempt === undefined){
+        return {
+          matches:false
+          };
+      } else {
+        var ret = {
+          matches: true,
+          captured_vars: {}
+        };
+        ret.captured_vars[name] = attempt;
+        return ret
+      }
+    }
+  }
+
   /* This or_rules function takes a set of rules and a name and returns a
   function that takes a string and returns {matches:false} if none of the rules
   match the input string and otherwise returns an object where matching is true
@@ -153,7 +206,10 @@
   it can cause problems. */
   var global_rules;
 
-  function exec_lark(var_name, rules) {
+  /* This will return a parser that executes a string (an expression) with the
+  given rules and captures that output with var_name. */
+
+  function expr_parser(var_name, rules) {
     return function(str_to_exec) {
 
       var older_rules;
@@ -183,22 +239,6 @@
   }
 
 
-  function named_parser(name, rule) {
-    return function(str_to_check) {
-      var attempt = rule(str_to_check);
-      if (attempt.matches) {
-        var ret = {
-          matches: true,
-          captured_vars: {}
-        };
-        ret.captured_vars[name] = str_to_check;
-        return ret;
-      }
-      return {
-        matches: false
-      };
-    }
-  }
 
   function make_rule(parser, postprocessor) {
     return {
@@ -208,6 +248,9 @@
   }
 
   function new_rule_parser(start) {
+    console.log(lark_functions);
+    nearley.Parser.prototype.lark=lark_functions;
+    // Does this 3rd argument of start work?
     return new nearley.Parser(grammar.ParserRules, grammar.ParserStart, start);
   }
 
@@ -216,7 +259,7 @@
   parsers). */
 
   function with_parser(parser) {
-    return rule_from_func(
+    return func_to_rule(
         function(str_to_parse) {
           var given_parser_attempt = parser(str_to_parse);
           if (!given_parser_attempt.matches) {
@@ -224,13 +267,13 @@
           }
           var str_form_of_parser = given_parser_attempt.captured_vars.parser;
           // Now I turn it into a parser
-          var rule_parser = new_rule_parser('parser');
+          var rule_parser = new_rule_parser('rule');
           rule_parsings = rule_parser.feed(str_form_of_parser).results;
           if (rule_parsings.length == 0) return undefined;
           var str_form_of_expr = given_parser_attempt.captured_vars.expr;
           var new_rule = rule_parsings[0](lark_functions);
           var temp_rules = global_rules.concat([new_rule]);
-          var attempt = exec_lark('out', temp_rules)(str_form_of_expr);
+          var attempt = expr_parser('out', temp_rules)(str_form_of_expr);
           if (attempt.matches) {
             return attempt.captured_vars.out;
           } else {
@@ -242,7 +285,7 @@
 
   /* This is for when you have a function that you want to use for both
     matching and postprocessing */
-  function rule_from_func(func_to_use) {
+  function func_to_rule(func_to_use) {
     return {
       parse: function(str_to_check) {
         var attempt = func_to_use(str_to_check);
@@ -266,12 +309,12 @@
 
   /* This takes the string form of a parser, such as $n!=($n-1)! and turns it
     into a parser object. */
-  function rule_from_str(str_to_convert) {
+  function str_to_rule(str_to_convert) {
     /* Is their anyway I can make it so I don't have to make a new js_parser
       each time? */
-    js_parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
+      var rule_parser = new_rule_parser('rule');
     // This will error is the input is not a valid parser.
-    return (js_parser.feed(str_to_convert).results[0])(lark_functions);
+    return (rule_parser.feed(str_to_convert).results[0])(lark_functions);
   }
 
   // MUST BE A MODULE
@@ -280,11 +323,12 @@
     join_parsers: join_parsers,
     concat_parsers: concat_parsers,
     or_rules: or_rules,
-    exec_lark: exec_lark,
+    expr_parser: expr_parser,
     named_parser: named_parser,
-    rule_from_func: rule_from_func,
+    func_to_parser: func_to_parser,
+    func_to_rule: func_to_rule,
     with_parser: with_parser,
-    rule_from_str: rule_from_str
+    str_to_rule: str_to_rule
   };
   //!== not needed
   if (typeof module != 'undefined' && typeof module.exports != 'undefined') {
